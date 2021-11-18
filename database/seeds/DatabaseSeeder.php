068b8e142f856eab66e97ff93cd24ae9e6039c3a
rodAlfaro-98 <rodrigoalfarod@gmail.com>
2021-11-17 20:34:20 -0600

    Se agregó el seeder de Cristobal

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(StartSeeder::class);
+        //$this->call(StartSeeder::class);
+        $this->call(CristobalSeeder::class);
     }
 }
