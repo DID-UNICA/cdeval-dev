44faecd73d103f0ac936db30c547586c3b8ebc1c
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-10-19 21:32:27 -0500

    Comentario eliminado

--- app/Http/Controllers/CoordinadorGeneralController.php
+++ app/Http/Controllers/CoordinadorGeneralController.php
@@ -1883,7 +1883,6 @@ $promedio_p4=[
       $criterio_recomendacion_arim = round($criterio_recomendacion_arim / $cursos->count(),2);
       $criterio_instructores_arim = round($criterio_instructores_arim / $cursos->count(),2);
 
-      // return $horarios;
       return view('pages.global')
       ->with('nombre_cursos',$nombre_cursos)
       ->with('periodo',$semestre.$periodo)
